02932f264f90173c4497e105228c28cbb7feadd4
leandro <leandros8w@gmail.com>
2022-09-21 01:44:15 -0300

    Versão concluida...

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3232;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
 app.engine('html', require('ejs').renderFile);
-app.set('view engine',  'html')
+app.set('view engine', 'html')
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, '/views'));
 
+let tarefas = ['Fazer algo', 'outra tarefa diferente', 'mais alguma coisa kkk'];
+
+app.post('/', (req, res) => {
+    tarefas.push(req.body.tarefa);
+    res.render('index', { tarefasList: tarefas });
+});
+
 app.get('/', (req, res) => {
-    res.render('index', {nome: 'joao pedro', msg: 'Nodejs é muito incrivel!'});
+    res.render('index', { tarefasList: tarefas });
+});
+
+app.get('/deletar/:id', (req, res) => {
+    tarefas = tarefas.filter((v, i) => {
+        if (i != req.params.id) return v;
+    })
+    res.render('index', { tarefasList: tarefas });
 });
 
 app.listen(PORT, () => {
